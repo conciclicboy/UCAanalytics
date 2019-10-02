7f01d7e5f3c091ce43f9c70b847d7e3fc351b3ae
Eduardo Arnoldo Aguilar Cañas <eduardo_canas@siman.com>
2019-10-02 17:11:41 -0600

    Actualiza

---
+++ update_fork.R
@@ -0,0 +1,35 @@
+# Sync a forked copy of INSEADAnalytics with the master project. For details,
+# see https://github.com/InseadDataAnalytics/INSEADAnalytics/issues/7
+
+if (!suppressWarnings(require(git2r))) {
+  install.packages("git2r", repos="http://cran.r-project.org/", quiet=TRUE)
+  library(git2r)
+}
+
+repo <- repository(".")
+if (!("upstream" %in% remotes(repo))) {
+  remote_add(repo, "upstream", "https://github.com/conciclicboy/UCAanalytics")
+}
+
+conf <- config(repo)
+if (is.null(conf$local$user.name) && is.null(conf$global$user.name)) {
+  message("Su usuario de Github username aún no ha sifo configurado para este repositorio.")
+  config(repo, user.name=readline("Github username: "))
+}
+if (is.null(conf$local$user.email) && is.null(conf$global$user.email)) {
+  message("Su email de Github aún no ha sifo configurado para este repositorio.")
+  config(repo, user.email=readline("Github email: "))
+}
+
+fetch(repo, "upstream")
+checkout(repo, "master")
+merge(repo, "upstream/master")
+
+message("
+  Su copia local de UCAanalytics está ahora en sync con el master project.
+  Usted puede actualizar su copia remota haciendo click en 'Push' en el panel de 'Git' panel o 
+  ejecutando:
+  
+  push(repo, credentials=cred_user_pass(readline('Github username: '), readline('Github password: ')))
+")
+# push(repo, credentials=cred_user_pass(readline("Github username: "), readline("Github password: ")))
